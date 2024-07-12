051b6c125770f349847a3543c069a1a8e4580e38
Priyanshu <sethipriyanshu@outlook.com>
2024-07-12 10:47:17 -0500

    finish maps section

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -3,8 +3,9 @@ package main
 type Dictionary map[string]string
 
 const (
-	ErrWordExists = DictionaryErr("cannot add word because it already exists")
-	ErrNotFound   = DictionaryErr("the word does not exist")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrNotFound         = DictionaryErr("the word does not exist")
+	ErrWordDoesNotExist = DictionaryErr("the word to update does not exist")
 )
 
 type DictionaryErr string
@@ -30,4 +31,19 @@ func (d Dictionary) Add(word, definition string) error {
 		return ErrWordExists
 	}
 	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
